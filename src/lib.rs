d32bfbdbcba45f4e2c2cb3f3bf6307af6ad468bf
Your Name <baylyd@email.arizona.edu>
2018-07-30 14:45:54 -0700

    push for acrichto to poke

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,12 @@
-#![feature(proc_macro, wasm_custom_section, wasm_import_module, wasm_serde_serialize)]
+#![feature(use_extern_macros, wasm_custom_section, wasm_import_module)]
+extern crate js_sys;
 #[macro_use(array)]
-// !! one issue is I'm running into difficulty in describing the types that will come out after the
-// !! fact
 extern crate ndarray;
 extern crate parser;
 extern crate wasm_bindgen;
 
 use parser::parse;
-
 use wasm_bindgen::prelude::*;
-use wasm_bindgen::js;
 use ndarray::prelude::*;
 use ndarray::Array;
 use ndarray::{ArrayD, Dim, Ix, Ix2, IxDyn};
@@ -34,8 +31,18 @@ impl Nd {
         let arr = Array::from_shape_vec(ixdyn, numbers).unwrap().into_dyn();
         Nd { array: arr }
     }
+    pub fn from_AB(arr: &js_sys::Float32Array) -> Nd {
+        let mut val_vec = vec![];
+        arr.for_each(&mut |x, _, _| {
+            val_vec.push(x);
+        });
+        let ixdyn = IxDyn(&[1_usize, val_vec.len()]);
+        Nd {
+            array: Array::from_shape_vec(ixdyn, val_vec).unwrap(),
+        }
+    }
     #[wasm_bindgen(constructor)]
-    pub fn make(arr_arg: &js::Array) -> Nd {
+    pub fn make(arr_arg: &js_sys::Array) -> Nd {
         // simple test
         let str_arr = String::from(arr_arg.to_string()); //both coversions required due to the JsString used first
         let mut vec_str: Vec<_> = str_arr.split(",").collect();
@@ -86,44 +93,3 @@ impl Nd {
     }
 }
 
-#[wasm_bindgen]
-pub struct NdArr {
-    array: Array<f32, Dim<[Ix; 1]>>,
-}
-
-#[wasm_bindgen]
-impl NdArr {
-    // ?? not sure what type this first dim should be
-    // ?? how should it look when we are passing a js array?
-    pub fn make(arr_arg: &js::Array) -> String {
-        // ?? transformations of arr_arg?
-        String::from(arr_arg.to_string())
-    }
-    pub fn show(&self) -> String {
-        format!("{:.10?}", self.array)
-    }
-    pub fn fill_f32(&mut self, val: f32) {
-        self.array.fill(val);
-    }
-}
-//?? need to come up with a way to wrap the actual objects rather than having to communicate
-//??through strings wasm_bindgen doesn't have a trait bound satisfied when trying to capture
-//??function return
-// ?? function that accepts ptr from javascript ??
-#[wasm_bindgen]
-pub fn get_array_rep() -> String {
-    make_array().array.to_string()
-}
-
-//?? can bindgen functions be used within other rust functions also?
-//?? making structs that are holders for types seems to be the only way to satisfy the wasm_bindgen
-//system?? is this expected ?? seems like it is much like the system whereby we create these
-//functions that are wasm_bindgen supported, and then make proper calls within them to other
-//library functions
-
-#[wasm_bindgen]
-pub fn make_array() -> NdArr {
-    NdArr {
-        array: Array::range(0., 12., 1.),
-    }
-}
