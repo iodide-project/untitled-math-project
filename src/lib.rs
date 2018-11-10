33da51dc58e19945d1c61ea87bdde104acba3915
Your Name <baylyd@email.arizona.edu>
2018-11-10 15:13:37 -0700

    incredible progress thanks to the num slices in wasm-bindgen

---
+++ src/lib.rs
@@ -0,0 +1,262 @@
+// next step is to try to figure out how to get the wasm module into an iodide session
+extern crate cfg_if; // this is the line which lest us enable things like wee aloc and console err panic hook
+extern crate wasm_bindgen;
+use wasm_bindgen::prelude::*;
+
+extern crate js_sys;
+#[macro_use(array)]
+extern crate ndarray;
+extern crate rand;
+extern crate ndarray_rand;
+
+
+
+
+use ndarray::prelude::*;
+use ndarray::SliceOrIndex;
+use ndarray::Array;
+use ndarray::{ArrayD, Dim, Ix, Ix2, IxDyn};
+use rand::{thread_rng,Rng};
+use rand::distributions::{Uniform,Distribution};
+use ndarray_rand::RandomExt;
+
+
+mod utils;
+
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, wasm-pack-tutorial!");
+}
+// need to be able to say the return type of  some of these functions is a certain dimension type
+// ?? should I use an enum for thedim type?
+#[wasm_bindgen]
+#[derive(Clone)]
+pub struct Nd {
+    array: ArrayD<f32>,
+}
+
+//setting the consolelog for faster debugging
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s:&str);
+}
+fn ret_f32(v:JsValue) -> f32{
+    js_sys::Number::new(&v).value_of() as f32
+}
+
+//todo change the ndarr to nd style
+#[wasm_bindgen]
+impl Nd {
+    // creation methods 
+
+    pub fn from_array_buffer(arr: &[f32], dims: &js_sys::Array) -> Nd {
+        let dim_vec = make_arr_usize(dims);
+        let ixdyn = IxDyn(&dim_vec);
+        Nd {
+            array: Array::from_shape_vec(ixdyn, arr.to_vec()).unwrap().into_dyn(),
+        }
+    }
+
+    // eye will return a zero matrix with ones on diagonal.
+    pub fn eye(dim:JsValue)-> Nd {
+        let int_val= js_sys::Number::new(&dim).value_of() as usize;
+        Nd {
+            array: Array::eye(int_val).into_dyn()
+        }
+    }
+
+    // A value which fills the array of dimensions given
+    pub fn fill(dims: &js_sys::Array,val:JsValue) -> Nd {
+        let dim_vec = make_arr_usize(dims);
+        let float_val = js_sys::Number::new(&val).value_of() as f32;
+        let ixdyn = IxDyn(&dim_vec);
+        Nd {
+            array: Array::from_elem(ixdyn,float_val)
+        }
+    }
+
+
+    pub fn range(start:JsValue,end:JsValue,step:JsValue)-> Nd{
+        let start = ret_f32(start);
+        let end = ret_f32(end);
+        let step = ret_f32(step);
+        Nd {
+            array:Array::range(start,end,step).into_dyn()
+        }
+    }
+
+    //assumption is that the elements are floats
+    pub fn linspace(start:JsValue,end:JsValue,len:JsValue)-> Nd{
+        //shadow the js
+        let start = ret_f32(start);
+        let end = ret_f32(end);
+        let len = ret_f32(len) as usize;
+        Nd {
+            array:Array::linspace(start,end,len).into_dyn()
+        }
+    }
+
+    pub fn zeros(dims: &js_sys::Array)-> Nd {
+        let dim_vec = make_arr_usize(dims);
+        let ixdyn = IxDyn(&dim_vec);
+        Nd {
+            array: Array::zeros(ixdyn)
+        }
+    }
+    #[wasm_bindgen(constructor)]
+    pub fn make(arr_arg: &js_sys::Array) -> Nd {
+        // simple test
+        let str_arr = String::from(arr_arg.to_string()); //both coversions required due to the JsString used first
+        let mut vec_str: Vec<_> = str_arr.split(",").collect();
+        let filler = vec_str.remove(0).parse::<f32>().unwrap();
+        let vec_dim: Vec<usize> = vec_str
+            .into_iter()
+            .map(|x| x.parse::<usize>().unwrap())
+            .collect();
+        let mut temp_arr = ArrayD::<f32>::zeros(IxDyn(&vec_dim));
+        // todo explore whether the from_elem alt is faster than zero/fill method
+        temp_arr.fill(filler);
+        Nd { array: temp_arr }
+    }
+
+    // would like to integrate random, but that is also a new crate to bring in
+    //pub fn random(dims: &js_sys::Array,range:&js_sys::Array) -> Nd {
+    //    let dim_vec = make_arr_usize(dims);
+    //    // !! investigatewhy the second for each is nec
+    //    log(&format!("{:?}",dim_vec));
+    //    let ixdyn = IxDyn(&dim_vec);
+    //    log(&format!("{:?}",ixdyn));
+    //    let range_vec = make_arr_f32(range);
+    //    log(&format!("{:?}",range_vec));
+    //    //let range_ob = Uniform::new(range_vec[0] as i32,range_vec[1]as i32);
+    //    let range_ob = Uniform::from(0..100);
+    //    log(&format!("{:?}",range_ob));
+    //    let a =  Array::random(ixdyn,range_ob) ;
+    //    log(&format!("{:?}",a));
+    //        Nd {
+    //            array:a.into_dyn()
+    //        }
+    //    }
+    // operator section
+    pub fn add(&self, other: &Nd) -> Nd {
+        let _temp_self = self.array.clone();
+        let _temp_other = other.array.clone();
+        Nd {
+            array: _temp_self + _temp_other,
+        }
+    }
+    pub fn dot(&self, other: &Nd) -> Nd {
+        let _temp_self = self.array.clone();
+        let _temp_other = other.array.clone();
+        let _temp_self_dottable = _temp_self.into_dimensionality::<Ix2>().unwrap();
+        let _temp_other_dottable = _temp_other.into_dimensionality::<Ix2>().unwrap();
+        Nd {
+            array: _temp_self_dottable.dot(&_temp_other_dottable).into_dyn(),
+        }
+    }
+    pub fn op(&self, operator: &str, other: &Nd) -> Nd {
+        let _temp_self = self.array.clone();
+        let _temp_other = other.array.clone();
+        // broadcast default to other broadcasting
+        let _broad_temp_other = _temp_other.broadcast(_temp_self.dim()).unwrap();
+        Nd {
+            array: match operator.trim() {
+                "+" => _temp_self + _broad_temp_other,
+                "-" => _temp_self - _broad_temp_other,
+                "*" => _temp_self * _broad_temp_other,
+                _ => panic!(),
+            },
+        }
+    }
+    pub fn get_slice_rust(&self, ind: JsValue) -> Self {
+        //ind is a string which will contain the unpackable indexing structure
+        //create a vector kind of thing from it, and rework following for iterating over the comma
+        //separated entries
+        let ind_string = ind.as_string().unwrap();
+        let ind_vector = ind_string.split(',').collect::<Vec<&str>>();
+        log(&format!("{:?}", ind_vector)[..]);
+        let mut val_vec: Vec<SliceOrIndex> = vec![];
+        for ind_str in ind_vector.iter() {
+            // single integer index specified
+            if let Ok(num) = ind_str.parse::<u32>() {
+                val_vec.push(SliceOrIndex::Index(num as isize));
+            } else {
+                // maybe extend this to the 2:5:1 syntax for indexing
+                let pair = ind_str
+                    .split(':')
+                    .map(|e| e.parse::<u32>())
+                    .collect::<Vec<Result<u32, std::num::ParseIntError>>>();
+                //make into slice for destructuring matching
+                match pair.as_slice() {
+                    //num: syntax
+                    [Ok(num), _] => {
+                        val_vec.push(SliceOrIndex::Slice {
+                            start: *num as isize,
+                            end: None,
+                            step: 1_isize,
+                        });
+                    }//num:num syntax
+                    [Ok(num1), Ok(num2)] => {
+                        val_vec.push(SliceOrIndex::Slice {
+                            start: *num1 as isize,
+                            end: Some(*num2 as isize),
+                            step: 1_isize,
+                        });
+                    }// :num syntax
+                    [_, Ok(num)] => {
+                        val_vec.push(SliceOrIndex::Slice {
+                            start: 0_isize,
+                            end: Some(*num as isize),
+                            step: 1_isize,
+                        });
+                    }
+                    _ => panic!(),
+                }
+            }
+        }
+        log(&format!("{:?}", val_vec)[..]);
+        // should have val_vec created by this point
+        // !! slicing creates an array view, which might not be accepted for ND creation
+        //      if so, look up how to create new ndarray from view
+        let nd_slice_ob = ndarray::SliceInfo::<_, IxDyn>::new(val_vec).unwrap();
+        Nd {
+            array: self.array.slice(nd_slice_ob.as_ref()).to_owned(),
+        }
+    }
+    pub fn get(&self, ind: &js_sys::Array) -> f32 {
+        let rust_ind = make_arr_usize(ind);
+        self.array[&rust_ind[..]]
+    }
+    pub fn set(&mut self, ind: &js_sys::Array, val: f32) {
+        let rust_ind = make_arr_usize(ind);
+        self.array[&rust_ind[..]] = val;
+    }
+    pub fn show(&self) -> String {
+        format!("{:?}", self.array)
+    }
+}
+
+fn make_arr_f32(arr:&js_sys::Array)-> Vec<f32> {
+    let mut v = vec![];
+    arr.for_each(&mut |x,_,_| v.push(js_sys::Number::new(&x).value_of() as f32));
+    v
+}
+
+fn make_arr_f64(arr:&js_sys::Array)-> Vec<f64> {
+    let mut v = vec![];
+    arr.for_each(&mut |x,_,_| v.push(js_sys::Number::new(&x).value_of()));
+    v
+}
+
+fn make_arr_usize(arr: &js_sys::Array) -> Vec<usize> {
+    let mut dim_vec = vec![];
+    arr.for_each(&mut |x, _, _| dim_vec.push(x.as_f64().unwrap() as usize));
+    dim_vec
+}
